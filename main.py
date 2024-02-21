cee36289bc69d8003a26a5b2e0708853e2a3725e
TatianaSham <156084120+TatianaSham@users.noreply.github.com>
2024-02-21 13:56:57 +0300

    Update main.py
    Capuccino

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import sys
-
+import sqlite3
 
 import PyQt5
 from PyQt5 import uic
 from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
-# from Main import
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 
 
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        PyQt5.uic.loadUi('main.ui', self)
-        self.initUI()
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.con = sqlite3.connect("coffee.sqlite")
+        self.pushButton.clicked.connect(self.update_result)
+        self.tableWidget.itemChanged.connect(self.item_changed)
+        self.pushButton_2.clicked.connect(self.save_results)
+        self.pushButton_3.clicked.connect(self.add_item)
+        self.lineEdit.setText('')
+        self.lineEdit_2.setText('')
+        self.lineEdit_3.setText('')
+        self.lineEdit_4.setText('')
+        self.lineEdit_5.setText('')
+        self.lineEdit_6.setText('')
+        self.lineEdit_7.setText('')
+        self.modified = {}
+        self.titles = None
 
-    def initUI(self):
+    '''def initUI(self):
         self.db = QSqlDatabase.addDatabase('QSQLITE')
         self.db.setDatabaseName('coffee.sqlite')
         self.db.open()
@@ -25,7 +37,72 @@ class Example(QMainWindow):
         self.model.select()
 
         self.view.setModel(self.model)
-        self.view.move(10, 10)
+        self.view.move(10, 10)'''
+
+    def update_result(self):
+        cur = self.con.cursor()
+        # Получили результат запроса, который ввели в текстовое поле
+        item_id = self.spinBox.text()
+        result = self.text.execute("SELECT * FROM Sorts WHERE id=?",
+                                   (item_id,)).fetchall()
+
+        # Заполнили размеры таблицы
+        self.tableWidget.setRowCount(len(result))
+        # Если запись не нашлась, то не будем ничего делать
+        if not result:
+            self.statusBar().showMessage('Ничего не нашлось')
+            return
+        else:
+            self.statusBar().showMessage(f"Нашлась запись с id = {item_id}")
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = [description[0] for description in cur.description]
+        # Заполнили таблицу полученными элементами
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+        self.modified = {}
+
+    def item_changed(self, item):
+        # Если значение в ячейке было изменено,
+        # то в словарь записывается пара: название поля, новое значение
+        self.modified[self.titles[item.column()]] = item.text()
+
+    def save_results(self):
+        if self.modified:
+            cur = self.con.cursor()
+            que = "UPDATE Sorts SET\n"
+            que += ", ".join([f"{key}='{self.modified.get(key)}'"
+                              for key in self.modified.keys()])
+            que += "WHERE id = ?"
+            print(que)
+            cur.execute(que, (self.spinBox.text(),))
+            self.con.commit()
+            self.modified.clear()
+
+    def add_item(self):
+        # Получим результат запроса,
+        # который ввели в текстовые поля
+        query = """INSERT INTO Sorts 
+                VALUES(self.lineEdit.text(),
+                 self.lineEdit_2.text(),
+                 self.lineEdit_3.text(),
+                 self.lineEdit_4.text(),
+                 self.lineEdit_5.text(),
+                 self.lineEdit_6.text(),
+                 self.lineEdit_1.text())"""
+        res = self.con.cursor().execute(query).fetchall()
+        # Заполним размеры таблицы
+        self.tableWidget.setColumnCount(len(res))
+        self.tableWidget.setRowCount(len(res[0]))
+        # Заполняем таблицу элементами
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+        self.con.commit()
+        self.modified.clear()
 
 
 if __name__ == '__main__':
